2648e228856fca2ee2bc1011dc898942dcab6eb1
Park junhyeok <85441841+kilito0118@users.noreply.github.com>
2025-04-01 00:22:49 +0900

    login_form
    login_form

---
+++ open_sw/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:open_sw/login/login_page.dart';
+
+Future<void> main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: LoginPage());
+  }
+}
