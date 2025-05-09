4d2d14a65e04cb641190f88a6d979290b41e6b7c
kilito0118 <kilito0118@naver.com>
2025-05-09 11:38:50 +0900

    .
    .

---
+++ open_sw/lib/mainPage/friends_screen/main_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+//import 'package:open_sw/naver_directions_button.dart';
+import 'friend_list_screen.dart';
+import 'group_list_screen.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+  final List<Widget> _screens = [GroupScreen(), FriendScreen()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        // 네비게이션바 (대충 구현함)
+        currentIndex: _currentIndex,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.group), label: '그룹'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: '친구'),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
