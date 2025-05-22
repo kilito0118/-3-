e767168e2c374ca2f30dabf02a2f8c6720ce1c84
kilito0118 <kilito0118@naver.com>
2025-05-22 13:57:50 +0900

    [기능 추가]그룹 화면 완성
    완성

---
+++ open_sw/lib/mainPage/groupPage/groupWidget/group_tile_widget.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class Group {
+  String name;
+  String leader;
+  String id;
+  List<String> members;
+  int memberCount;
+  Group({
+    required this.id,
+    required this.name,
+    required this.leader,
+    required this.members,
+    required this.memberCount,
+  });
+}
+
+class GroupTileWidget extends StatelessWidget {
+  final Group group;
+  const GroupTileWidget({super.key, required this.group});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: () {
+            print(1);
+          },
+          child: Container(
+            height: 80,
+            margin: EdgeInsets.only(right: 8), // 아이콘과 간격
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.white,
+            ),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            // 그룹 프로필 (지금은 그냥 이름 첫글자로 해둠)
+                            CircleAvatar(
+                              backgroundColor: Colors.black,
+                              /*
+                            child: SizedBox(
+                              width: 40,
+                              height: 40,*/
+                              child: Text(
+                                group.name[0],
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+
+                            SizedBox(width: 10),
+
+                            // 친구 이름
+                            Text(
+                              group.name,
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        SizedBox(height: 10), // tile별 거리 확보
+      ],
+    );
+  }
+}
--- open_sw/lib/mainPage/groupPage/group_page.dart
+++ open_sw/lib/mainPage/groupPage/group_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:open_sw/mainPage/groupPage/groupWidget/group_plus_tile_widget.dart';
+import 'package:open_sw/mainPage/groupPage/groupWidget/group_tile_widget.dart';
 
 class GroupPage extends StatefulWidget {
   const GroupPage({super.key, required this.userName});
@@ -8,33 +10,124 @@ class GroupPage extends StatefulWidget {
 }
 
 class _GroupPageState extends State<GroupPage> {
+  final List<Group> exampleGroups = [
+    Group(
+      id: "Qweeafs",
+      name: 'Flutter Developers',
+      leader: 'Alice',
+      members: ['Alice', 'Bob', 'Charlie', 'Diana'],
+      memberCount: 4,
+    ),
+    Group(
+      id: "Qwe",
+      name: 'AI Study Club',
+      leader: 'Eve',
+      members: ['Eve', 'Frank', 'Grace'],
+      memberCount: 3,
+    ),
+    Group(
+      id: "Qwe5",
+      name: 'Running Team',
+      leader: 'Henry',
+      members: ['Henry', 'Ivy', 'Jack', 'Kelly', 'Leo'],
+      memberCount: 5,
+    ),
+    Group(
+      id: "Qwe3",
+      name: 'Book Lovers',
+      leader: 'Mona',
+      members: ['Mona', 'Nate'],
+      memberCount: 2,
+    ),
+    Group(
+      id: "Qwe2",
+      name: 'Music Band',
+      leader: 'Oscar',
+      members: ['Oscar', 'Paul', 'Quinn', 'Rita'],
+      memberCount: 4,
+    ),
+  ];
+
+  //List<Group> groups;
+  final List<int> colorCodes = <int>[600, 500, 100];
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Column(
         children: [
+          SizedBox(height: 80),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               SizedBox(height: 82, width: 26),
               Text(
                 "안녕하세요,\n${widget.userName}님.",
-                style: Theme.of(context).textTheme.bodyLarge,
+                style: TextStyle(fontSize: 32, fontWeight: FontWeight.w400),
                 textAlign: TextAlign.center,
               ),
+              SizedBox(width: 83),
               Icon(Icons.account_circle_rounded, size: 68),
-              /*
-              ClipOval(
-                child: Image(
-                  image: AssetImage(["assets/images/", log].join()),
-                  height: 100.0,
-                  width: 100.0,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              */
             ],
           ),
+          SizedBox(height: 40),
+          Padding(
+            padding: EdgeInsets.only(left: 26, right: 26),
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                    children: [
+                      Text(
+                        "나의 그룹",
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {},
+                        child: Text(
+                          "편집",
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      ListView.builder(
+                        physics: NeverScrollableScrollPhysics(),
+
+                        primary: false,
+                        shrinkWrap: true,
+                        padding: const EdgeInsets.only(bottom: 10),
+                        itemCount: exampleGroups.length + 1,
+                        itemBuilder: (BuildContext context, int index) {
+                          if (index < exampleGroups.length) {
+                            return GroupTileWidget(group: exampleGroups[index]);
+                          } else {
+                            return GroupPlusTileWidget();
+                          }
+                        },
+                      ),
+                      SizedBox(height: 140),
+                      Text(""),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 140),
+          Text(""),
         ],
       ),
     );
