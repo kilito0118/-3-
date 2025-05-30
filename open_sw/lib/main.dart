157b69a02bb1aee4ac48b0fe92ea3f88185a6e1b
apri1f00ls <wkdduddn7321@gmail.com>
2025-05-31 00:44:18 +0900

    placeFunctionAdded
    place_finding_functions_added

--- open_sw/lib/main.dart
+++ open_sw/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:open_sw/login/login_screen.dart';
 
 //import 'package:open_sw/naver_directions_button.dart';
@@ -8,6 +9,7 @@ import 'firebase_options.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await dotenv.load();
   runApp(MyApp());
 }
 
