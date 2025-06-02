dd4f93b1327ebd0cbd46e256c95c293e2847d926
apri1f00ls <wkdduddn7321@gmail.com>
2025-06-02 11:10:19 +0900

    colorchanged
    장소 위젯 색 변경

--- open_sw/lib/recommendPlacePage/recommended_place_widget.dart
+++ open_sw/lib/recommendPlacePage/recommended_place_widget.dart
@@ -54,7 +54,6 @@ class _RecommendedPlaceWidgetState extends State<RecommendedPlaceWidget> {
       child: Container(
         clipBehavior: Clip.antiAlias,
         decoration: BoxDecoration(
-            color: Colors.white.withAlpha(210),
             borderRadius: BorderRadius.circular(20),
             boxShadow: [
               BoxShadow(
@@ -69,151 +68,156 @@ class _RecommendedPlaceWidgetState extends State<RecommendedPlaceWidget> {
         child: ClipRRect(
           child: BackdropFilter(
             filter: ImageFilter.blur(sigmaX: 40, sigmaY: 40),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        width: 160,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              widget.place['name'] ?? '',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 18,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white.withAlpha(144),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: 160,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                widget.place['name'] ?? '',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                ),
+                                softWrap: true,
+                                maxLines: null,
+                                overflow: TextOverflow.visible,
                               ),
-                              softWrap: true,
-                              maxLines: null,
-                              overflow: TextOverflow.visible,
+                              Text(
+                                widget.place['address'] ?? '',
+                                style: TextStyle(
+                                  color: Colors.grey[600],
+                                  fontSize: 14,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+
+                        GestureDetector(
+                          onTapDown: _addTapDown,
+                          onTapUp: _addTapUp,
+                          onTapCancel: _addTapCancel,
+                          child: AnimatedScale(
+                            scale: _scale,
+                            duration: const Duration(milliseconds: 400),
+                            curve: Curves.easeOutBack,
+                            child: Container(
+                                margin: EdgeInsets.only(top: 4),
+                                width: 100,
+                                height: 28,
+                                decoration: BoxDecoration(
+                                    gradient: LinearGradient(
+                                        colors: [
+                                          const Color(0xFFFF9933), const Color(0xFFFF6600)
+                                        ],
+                                        begin: Alignment.centerLeft,
+                                        end: Alignment.centerRight
+                                    ),
+                                    borderRadius: BorderRadius.circular(10),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Color(0x7FFF9933),
+                                        blurRadius: 10,
+                                        offset: Offset(0, 4),
+                                        spreadRadius: 0,
+                                      )
+                                    ]
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    '일정에 등록',
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 16
+                                    ),
+                                  ),
+                                )
                             ),
-                            Text(
-                              widget.place['address'] ?? '',
-                              style: TextStyle(
-                                color: Colors.grey[600],
-                                fontSize: 14,
-                              ),
-                            )
+                          ),
+                        ),
+
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    height: 1,
+                    decoration: BoxDecoration(
+                        color: Colors.black.withAlpha(20)
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(),
+
+                      TextButton(
+                        style: TextButton.styleFrom(
+                            foregroundColor: _phone.isNotEmpty ? Color(0xFF52A658) : Colors.grey,
+                            padding: EdgeInsets.all(0),
+                            minimumSize: Size(144, 44)
+                        ),
+                        onPressed: () {
+                          if(_phone.isNotEmpty){
+                            _launchUrl('tel:$_phone');
+                          }
+                          else{
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text('전화번호가 없습니다.')),
+                            );
+                          }
+                        },
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('전화걸기', style: TextStyle(fontSize: 14),),
+                            SizedBox(width: 4,),
+                            Icon(Icons.add_call, size: 20,),
                           ],
                         ),
                       ),
-
-                      GestureDetector(
-                        onTapDown: _addTapDown,
-                        onTapUp: _addTapUp,
-                        onTapCancel: _addTapCancel,
-                        child: AnimatedScale(
-                          scale: _scale,
-                          duration: const Duration(milliseconds: 400),
-                          curve: Curves.easeOutBack,
-                          child: Container(
-                              margin: EdgeInsets.only(top: 4),
-                              width: 100,
-                              height: 28,
-                              decoration: BoxDecoration(
-                                  gradient: LinearGradient(
-                                      colors: [
-                                        const Color(0xFFFF9933), const Color(0xFFFF6600)
-                                      ],
-                                      begin: Alignment.centerLeft,
-                                      end: Alignment.centerRight
-                                  ),
-                                  borderRadius: BorderRadius.circular(10),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Color(0x7FFF9933),
-                                      blurRadius: 10,
-                                      offset: Offset(0, 4),
-                                      spreadRadius: 0,
-                                    )
-                                  ]
-                              ),
-                              child: Center(
-                                child: Text(
-                                  '일정에 등록',
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 16
-                                  ),
-                                ),
-                              )
-                          ),
+                      TextButton(
+                        style: TextButton.styleFrom(
+                            foregroundColor: Colors.black,
+                            padding: EdgeInsets.all(0),
+                            minimumSize: Size(144, 44)
+                        ),
+                        onPressed: () {
+                          final url = "https://place.map.kakao.com/${widget.place['id']}";
+                          _launchUrl(url);
+                        },
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('지도에서 보기', style: TextStyle(fontSize: 14),),
+                            SizedBox(width: 4,),
+                            Icon(Icons.open_in_new, size: 20,),
+                          ],
                         ),
                       ),
-
+                      SizedBox(),
                     ],
                   ),
-                ),
-                Container(
-                  width: double.infinity,
-                  height: 1,
-                  decoration: BoxDecoration(
-                      color: Colors.black.withAlpha(20)
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(),
-
-                    TextButton(
-                      style: TextButton.styleFrom(
-                          foregroundColor: _phone.isNotEmpty ? Color(0xFF52A658) : Colors.grey,
-                          padding: EdgeInsets.all(0),
-                          minimumSize: Size(144, 44)
-                      ),
-                      onPressed: () {
-                        if(_phone.isNotEmpty){
-                          _launchUrl('tel:$_phone');
-                        }
-                        else{
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(content: Text('전화번호가 없습니다.')),
-                          );
-                        }
-                      },
-                      child: Row(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text('전화걸기', style: TextStyle(fontSize: 14),),
-                          SizedBox(width: 4,),
-                          Icon(Icons.add_call, size: 20,),
-                        ],
-                      ),
-                    ),
-                    TextButton(
-                      style: TextButton.styleFrom(
-                          foregroundColor: Colors.black,
-                          padding: EdgeInsets.all(0),
-                          minimumSize: Size(144, 44)
-                      ),
-                      onPressed: () {
-                        final url = "https://place.map.kakao.com/${widget.place['id']}";
-                        _launchUrl(url);
-                      },
-                      child: Row(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text('지도에서 보기', style: TextStyle(fontSize: 14),),
-                          SizedBox(width: 4,),
-                          Icon(Icons.open_in_new, size: 20,),
-                        ],
-                      ),
-                    ),
-                    SizedBox(),
-                  ],
-                ),
-              ],
-            ),
+                ],
+              ),
+            )
           ),
         ),
       ),
